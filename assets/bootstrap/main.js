01995a732e6f03b181296345ade8b58b51c6c80d
Eric Barnes <eric@ericlbarnes.com>
2012-02-15 16:41:29 -0500

    Fixed navigation and added slight fade image to content block

--- assets/bootstrap/main.js
+++ assets/bootstrap/main.js
@@ -1,21 +1,11 @@
 $(document).ready(function() {
-	$.fn.slideFadeToggle = function(speed, easing, callback) {
-		return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
-	};
-
 	$('.sidebar ul ul').hide();
-
 	$('.sidebar ul ul').each(function() {
 		$(this).parent('li').addClass('close');
-	});
-
-	$('.sidebar li a').click(function(){
-		if ($(this).parent('li').hasClass('open')) {
-			$(this).parent('li').removeClass('open').addClass('close');
-		} else {
-			$(this).parent('li').removeClass('close').addClass('open');
-		}
-		$(this).next('ul').slideFadeToggle('slow').show();
-		return false;
+		$(this).prev('a').bind('click', function() {
+			$(this).parent('li').toggleClass('open').toggleClass('close');
+			$(this).next('ul').animate({opacity: 'toggle', height: 'toggle'}, "slow");
+			return false;
+		});
 	});
 });
