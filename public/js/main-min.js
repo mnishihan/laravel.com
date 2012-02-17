bf93e54c59218e5c2f07ae6167fbe04dd74d47f0
Eric Barnes <eric@ericlbarnes.com>
2012-02-16 22:07:47 -0500

    Added js to keep sidebar open when clicking.

--- public/js/main-min.js
+++ public/js/main-min.js
@@ -95,9 +95,19 @@
 ***********************************************/ 
 
 $(document).ready(function() {
+	var url = document.location.href;
+	var active = url.substr(0, url.length-document.location.hash.length);
+	//console.log(active);
 	$('.sidebar ul ul').hide();
 	$('.sidebar ul ul').each(function() {
 		$(this).parent('li').addClass('close');
+		// console.log($(this));
+		anchor = $(this).prev('a').attr('href');
+		if (anchor == active) {
+			$(this).prev('a').addClass('active');
+			$(this).parent('li').addClass('open').removeClass('close');
+			$(this).show();
+		}
 		$(this).prev('a').bind('click', function() {
 			$(this).parent('li').toggleClass('open').toggleClass('close');
 			$(this).next('ul').animate({opacity: 'toggle', height: 'toggle'}, "slow");
