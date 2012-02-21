8a6ce3cd4ff8f17b040a03e08b3d44fa01b5008f
Eric Barnes <eric@ericlbarnes.com>
2012-02-20 21:01:42 -0500

    Moved search bar to sidebar so it matches the bundles

--- application/views/layouts/docs.php
+++ application/views/layouts/docs.php
@@ -3,14 +3,20 @@
 <div class="container main">
 	<div class="row">
 		<div class="sidebar docs span3 <?php echo $section ?>">
+			<form method="get" action="http://www.google.com/search">
+				<input type="hidden" name="as_sitesearch" id="as_sitesearch" value="laravel.com/docs/">
+				<input type="search" results="5" name="q" id="q" placeholder="Search the documentation">
+			</form>
 			<?php echo $sidebar ?>
 		</div>
 		<div class="content docs span9">
 			<div class="well">
+				<!--
 				<form method="get" action="http://www.google.com/search">
 					<input type="hidden" name="as_sitesearch" id="as_sitesearch" value="laravel.com/docs/">
 					<input type="search" results="5" name="q" id="q" placeholder="Search the documentation">
 				</form>
+				-->
 
 				<?php echo $content; ?>
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
 
 	$('.docs.sidebar ul ul').hide();
 	$('.docs.sidebar ul ul').each(function() {
-		$(this).parent('li').addClass('close');
+		$(this).parent('li').addClass('nav-close');
 		var anchor = $(this).prev('a').attr('href');
 		if (anchor == active) {
 			$(this).prev('a').addClass('active');
-			$(this).parent('li').addClass('open').removeClass('close');
+			$(this).parent('li').addClass('nav-open').removeClass('nav-close');
 			$(this).show();
 		} else if (anchor == parent_folder) {
 			$(this).prev('a').addClass('active');
-			$(this).parent('li').addClass('open').removeClass('close');
+			$(this).parent('li').addClass('nav-open').removeClass('nav-close');
 			$(this).show();
 		}
 		$(this).prev('a').bind('click', function(e) {
-			$(this).parent('li').toggleClass('open').toggleClass('close');
+			$(this).parent('li').toggleClass('nav-open').toggleClass('nav-close');
 			$(this).next('ul').animate({opacity: 'toggle', height: 'toggle'}, "slow");
 			return false;
 		});
