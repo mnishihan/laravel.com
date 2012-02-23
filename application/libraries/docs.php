97f9557144e038c7fd3c2be70f3b3ca8e9e257b0
M N Islam Shihan <mnishihan@gmail.com>
2012-02-24 04:12:18 +0600

    Reverted converted EOLs for windows platform to Unix EOLs for consistency

--- application/libraries/docs.php
+++ application/libraries/docs.php
@@ -14,50 +14,50 @@
  */
 class Docs {
 
-	/**
-	 * Title
-	 *
-	 * Generate a title from the markdown file
-	 *
-	 * @param  string $content
-	 * @return string
-	 */
-	public static function title($content)
-	{
-		if ($title = static::text_between_tag($content))
-		{
-			return $title[0]. ' - ';
-		}
-		return null;
-	}
+    /**
+     * Title
+     *
+     * Generate a title from the markdown file
+     *
+     * @param  string $content
+     * @return string
+     */
+    public static function title($content)
+    {
+        if ($title = static::text_between_tag($content))
+        {
+            return $title[0]. ' - ';
+        }
+        return null;
+    }
 
-	/**
-	 * Text Between Tag
-	 *
-	 * Parse a string and return the text inside an html tag
-	 *
-	 * @param  string $string
-	 * @param  string $tag
-	 * @return string
-	 */
-	protected static function text_between_tag($string, $tag = 'h1')
-	{
-		$pattern = "/<$tag>(.*?)<\/$tag>/";
-		preg_match_all($pattern, $string, $matches);
-		return $matches[1];
-	}
+    /**
+     * Text Between Tag
+     *
+     * Parse a string and return the text inside an html tag
+     *
+     * @param  string $string
+     * @param  string $tag
+     * @return string
+     */
+    protected static function text_between_tag($string, $tag = 'h1')
+    {
+        $pattern = "/<$tag>(.*?)<\/$tag>/";
+        preg_match_all($pattern, $string, $matches);
+        return $matches[1];
+    }
 
-	/**
-	 * Content
-	 *
-	 * Load the content based on the section and page in the uri.
-	 *
-	 * @param  string $section
-	 * @param  string $page
-	 * @return string
-	 */
-	public static function content($section = null, $page = null)
-	{
+    /**
+     * Content
+     *
+     * Load the content based on the section and page in the uri.
+     *
+     * @param  string $section
+     * @param  string $page
+     * @return string
+     */
+    public static function content($section = null, $page = null)
+    {
         $markdown_file = !empty($section) && !empty($page)
             ?$section . DS . $page
             :(
@@ -87,7 +87,7 @@ class Docs {
                     || !is_int($cached_data['mtime'])
                     || (filemtime($markdown_path) != $cached_data['mtime']);
 
-		if($refresh_cache){
+        if($refresh_cache){
             $cached_data = array();
             $cached_data['content'] = MarkdownExtended(File::get($markdown_path));
             $cached_data['content'] = str_replace('/docs', URL::to('docs'), $cached_data['content']);
@@ -95,5 +95,5 @@ class Docs {
             Cache::put($cache_key,$cached_data,60*24*365); // cache the content for 1 year
         }
         return $cached_data['content'];
-	}
+    }
 }
